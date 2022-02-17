dcf939f2c00a395a18ed945a0357bcda82566598
Sooraj Parameswaran <srjpn123@gmail.com>
2022-02-17 21:08:43 +0530

    optimised solution

--- lib.js
+++ lib.js
@@ -1,8 +1,14 @@
 exports.timeConversion = (s) => {
     const repeatRegex = /(\d+):(\d+):(\d+)(\w+)/g
-    var match = repeatRegex.exec(s);
-    var hours = match[4].toLowerCase() == 'pm' ? (Number.parseInt(match[1]) + 12) % 24 : (Number.parseInt(match[1])) % 12
-    var result = `${hours.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}:${match[2]}:${match[3]}`
-    return result
+    var [_, hours, minutes, seconds, ampm] = repeatRegex.exec(s);
 
+    var hours = ampm.toLowerCase() == 'pm'
+        ? ((Number.parseInt(hours) % 12) + 12) % 24
+        : (Number.parseInt(hours)) % 12
+
+    return [pad(hours), minutes, seconds].join(':');
+}
+
+const pad = (x) => {
+    return String("0" + x).slice(-2);
 }
--- lib.test.js
+++ lib.test.js
@@ -5,4 +5,17 @@ describe('timeConversion', () => {
     it('should convert 07:05:45PM to 24 hour format', () => {
         expect(timeConversion('07:05:45PM')).to.be.eql('19:05:45');
     })
+
+    it('should convert 12:45:54PM to 24 hour format', () => {
+        expect(timeConversion('12:45:54PM')).to.be.eql('12:45:54');
+    })
+    
+    it('should convert 12:40:22AM to 24 hour format', () => {
+        expect(timeConversion('12:40:22AM')).to.be.eql('00:40:22');
+    })
+
+    it('should convert 12:00:00AM to 24 hour format', () => {
+        expect(timeConversion('12:00:00AM')).to.be.eql('00:00:00');
+    })
 });
+
